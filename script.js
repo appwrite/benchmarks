bfa9a48d647363d18b3f7a48451c72d69ab57847
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-05-06 11:54:50 +0200

    refactor benchmark logic

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const APPWRITE_KEY = __ENV.KEY || "";
 
 const CONFIG_VUS = parseInt(__ENV.VUS) || 10;
 const CONFIG_SEED = parseInt(__ENV.SEED) || 123456789;
-const CONFIG_DURATION = __ENV.DURATION || "10s";
+const CONFIG_ITERATIONS = parseInt(__ENV.ITERATIONS) || 5;
 
 if (
     APPWRITE_ENDPOINT === "" ||
@@ -18,8 +18,15 @@ if (
 }
 
 export let options = {
-    vus: CONFIG_VUS,
-    duration: CONFIG_DURATION,
+    discardResponseBodies: false,
+    scenarios: {
+        contacts: {
+            executor: "per-vu-iterations",
+            vus: CONFIG_VUS,
+            iterations: CONFIG_ITERATIONS,
+            maxDuration: "10m",
+        },
+    },
 };
 
 export const setup = () => {
